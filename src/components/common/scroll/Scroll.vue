57b78a0b1537813eebc3539800c773be0f8f6fc0
keavlun <keavlun@163.com>
2019-12-21 19:20:17 +0800

    update03

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -15,24 +15,39 @@ export default {
     };
   },
   components: {},
-  props:{
-    probeType:{
+  props: {
+    probeType: {
       type: Number,
-      defalut:1
+      defalut: 1
+    },
+    pullUpLoad: {
+      type: Boolean,
+      defalut: false
     }
   },
   mounted() {
     this.scroll = new BScroll(this.$refs.wrapper, {
       click: true,
-      probeType:this.probeType
-    });
-    this.scroll.on('scroll',(position) => {
-      console.log(position);
+      probeType: this.probeType,
+      pullUpLoad: this.pullUpLoad
+    })
+
+    this.scroll.on("scroll", position => {
+      this.$emit("scroll", position);
+    })
+
+    this.scroll.on('pullingUp', () => {
+      // console.log('123');
+      this.$emit('pullingUp')
     })
   },
-  methods:{
-    scrollTo(x,y,time=500){
-      this.scroll.scrollTo(x,y,time);
+
+  methods: {
+    scrollTo(x, y, time = 500) {
+      this.scroll.scrollTo(x, y, time);
+    },
+    finishPullUp(){
+      this.scroll.finishPullUp();
     }
   }
 };
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,14 +3,19 @@
     <nav-bar class="home-navbar">
       <div slot="center">购物街</div>
     </nav-bar>
-    <scroll class="content" ref="scroll" :probe-type="3">
+    <scroll class="content" 
+            ref="scroll" 
+            :probe-type="3" 
+            @scroll="topShow" 
+            :pull-up-load="true" 
+            @pullingUp="loadMore">
       <home-swiper :banners="banners" />
       <recommend-view :recommends="recommends" />
       <future-view />
       <tab-control :titles="['流行','新款','精选']" class="tab-bar" @tabClick="tabClick" />
       <goods-list :goods="togglelist" />
     </scroll>
-    <back-top @click.native="backClick"></back-top>
+    <back-top @click.native="backClick" v-show="isShow"></back-top>
   </div>
 </template>
 
@@ -22,7 +27,7 @@ import FutureView from "./childComps/FutureView";
 import TabControl from "../../components/content/tabControl/TabControl";
 import GoodsList from "../../components/content/goods/GoodsList";
 import Scroll from "../../components/common/scroll/Scroll";
-import BackTop from '../../components/common/backtop/BackTop';
+import BackTop from "../../components/common/backtop/BackTop";
 
 import { getHomeMultidata, getHomeGoods } from "../../network/home";
 
@@ -36,7 +41,8 @@ export default {
         new: { page: 0, list: [] },
         sell: { page: 0, list: [] }
       },
-      goodstype: "pop"
+      goodstype: "pop",
+      isShow: null
     };
   },
   created() {
@@ -57,10 +63,11 @@ export default {
       getHomeGoods(type, page).then(res => {
         this.goods[type].list.push(...res.data.list);
         this.goods[type].page += 1;
+
+        this.$refs.scroll.finishPullUp()
       });
     },
     tabClick(index) {
-      console.log(index);
       switch (index) {
         case 0:
           this.goodstype = "pop";
@@ -73,9 +80,16 @@ export default {
           break;
       }
     },
-    backClick(){
-      this.$refs.scroll.scrollTo(0,0);
-    }
+    backClick() {
+      this.$refs.scroll.scrollTo(0, 0);
+    },
+    topShow(position) {
+      this.isShow = (-position.y) > 1000;
+    },
+    loadMore(){
+      console.log('123123');
+      this.getHomeGoods(this.goodstype)
+    },
   },
   components: {
     NavBar,
@@ -114,7 +128,7 @@ export default {
   position: sticky;
   top: 44px;
 }
-.content{
+.content {
   height: calc(100% - 93px);
   overflow: hidden;
   margin-top: 44px;
